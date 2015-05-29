7c04430db0a9713b5318454773c017b09dae40e5
David Garber <davidgarber@gmail.com>
2015-05-29 11:46:51 -0700

    work on front end. does not display results.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,21 @@ var findAndReplace = function(stringSearch, wordContain, wordReplace){
   stringSearch = stringSearch.toLowerCase().replace(wordContain, wordReplace);
   return stringSearch;
 };
+
+$(document).ready(function() {
+  $("form#find-replace").submit(function(event) {
+    var string = $("input#string").val();
+    var contain = $("input#contain").val();
+    var replace = $("input#replace").val();
+    var result = findAndReplace(contain, replace);
+
+    $("#contain").text(contain);
+    if (!contain) {
+      $(".not").text("not");
+    }
+
+    $("result-wrapper").show();
+    $("#result").text(result);
+    event.preventDefault();
+  });
+});

58dd66603448bd80919e6d9a4eed2fc2c4e0904c
David Garber <davidgarber@gmail.com>
2015-05-29 13:55:40 -0700

    changes third spec again. return original string if a word is not in the string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var findAndReplace = function(stringSearch, wordContain, wordReplace){
 
   if (stringSearch.toLowerCase().includes(wordContain)) {
-  stringSearch = stringSearch.toLowerCase().replace(wordContain, wordReplace);
-  return stringSearch; } else
-  {return "does not exist";}
+  var stringSearchResult = stringSearch.toLowerCase().replace(wordContain, wordReplace);
+  return stringSearchResult; } else
+  {return stringSearch;}
 };
 
 $(document).ready(function() {
@@ -12,6 +12,11 @@ $(document).ready(function() {
     var contain = $("input#contain").val();
     var replace = $("input#replace").val();
     var result = findAndReplace(string, contain, replace);
+
+    $(".contain").text(contain);
+    if(false) {
+      $(".not").text(" is not in this string.")
+    }
 
     $("result-wrapper").show();
     $("#result").text(result);
--- spec/specs.js
+++ spec/specs.js
@@ -7,7 +7,10 @@ describe("findAndReplace", function() {
     expect(findAndReplace("Hello WoRlD", "world", "country")).to.equal("hello country");
   });
 
-  it("returns 'does not exist' if a the word is not in the string", function() {
-    expect(findAndReplace("Hello world", "city", "country")).to.equal("does not exist");
+  // it("returns 'This word is not in this string' if a the word is not in the string", function() {
+  //   expect(findAndReplace("Hello world", "city", "country")).to.equal("This word is not in this string.");
+  // });
+  it("returns original string if a the word is not in the string", function() {
+    expect(findAndReplace("Hello world", "city", "country")).to.equal("Hello world");
   });
 });
